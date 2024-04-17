99873d464e3dfe03247d55dbe04ecb6fc9ed2862
ramimikho <rami.sa.mikho@gmail.com>
2024-04-17 12:11:51 +1000

    functionality of buttons+sound+save+constraintlayout

--- app/src/main/java/com/rami/assignmentapplication/MainActivity.kt
+++ app/src/main/java/com/rami/assignmentapplication/MainActivity.kt
@@ -2,12 +2,17 @@ package com.rami.assignmentapplication
 
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentScore : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,8 +22,55 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val AddScore = findViewById<Button>(R.id.AddScore)
+        val ReduceScore = findViewById<Button>(R.id.ReduceScore)
+        val Reset = findViewById<Button>(R.id.Reset)
+        val mediaPlayer = MediaPlayer.create(this, R.raw.wind_chimes)
+
+        val Score = findViewById<TextView>(R.id.Score)
+
+        savedInstanceState?.let {
+            // If there is, retrieve the score
+            currentScore = savedInstanceState.getInt("currentScore", 0)
+            Score.text = "Score: $currentScore"
+        }
+
+        AddScore.setOnClickListener {
+            increment()
+            Score.text = "Score: $currentScore"
+            if(currentScore >= 15){
+                mediaPlayer.start()
+            }
+        }
+        ReduceScore.setOnClickListener{
+            decrement()
+            Score.text = "Score: $currentScore"
+        }
+        Reset.setOnClickListener{
+            currentScore = 0
+            Score.text = "Score: $currentScore"
+        }
+
+
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        // Save the current score to the outState bundle
+        outState.putInt("currentScore", currentScore)
+        super.onSaveInstanceState(outState)
+    }
+    private fun increment() {
+        currentScore += 1
+        //Toast.makeText(this, "Score: $currentScore", Toast.LENGTH_SHORT).show()
+    }
+    private fun decrement() {
+        if (currentScore > 0) {
+            currentScore -= 1
+            //Toast.makeText(this, "Score: $currentScore", Toast.LENGTH_SHORT).show()
+        }
     }
 
-    //var mediaPlayer = MediaPlayer.create(context, R.raw.wind_chimes)
+
+    //
     //mediaPlayer.start()
 }
